2bf77f233320c5e013611bbf8b5f715b7d332a69
Mickael <mickaeldesclaux@gmail.com>
2024-04-24 19:16:28 +0200

    Initialize MAUI app

---
+++ MyBookshelf.App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MyBookshelf.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MyBookshelf.App/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MyBookshelf.App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
