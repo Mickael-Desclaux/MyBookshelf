587fa2fedd9ce95466b111bce369526aca5c18f6
Mickael <mickaeldesclaux@gmail.com>
2024-04-25 16:58:22 +0200

    Handle AddBook form

---
+++ MyBookshelf.App/Components/Pages/AddBookBase.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using MyBookshelf.Core.Model;
+using MyBookshelf.Core.Service;
+
+namespace MyBookshelf.App.Components.Pages
+{
+    public class AddBookBase : ComponentBase
+    {
+        [Inject] protected IBookService BookService { get; init; } = default!;
+
+        protected Book Book = new();
+
+        protected string newQuote;
+
+        protected EditContext EditContext;
+
+        protected async Task AddBookAsync()
+        {
+            try
+            {
+                if (EditContext.Validate())
+                {
+                    Book.Quotes = FormatQuotes(Book);
+                    await BookService.AddBookAsync(Book);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException);
+            }
+        }
+
+        protected List<string> GetQuotes(Book book)
+        {
+            if (!string.IsNullOrWhiteSpace(book.Quotes))
+            {
+                return book.Quotes.Split('|').ToList();
+            }
+            else
+            {
+                return new List<string>();
+            }
+        }
+
+        protected void AddNewQuote()
+        {
+            if (!string.IsNullOrWhiteSpace(newQuote))
+            {
+                if (string.IsNullOrWhiteSpace(Book.Quotes))
+                {
+                    Book.Quotes = newQuote;
+                }
+                else
+                {
+                    Book.Quotes += "|" + newQuote;
+                }
+                newQuote = string.Empty;
+            }
+        }
+
+        protected string FormatQuotes(Book book)
+        {
+            if (string.IsNullOrWhiteSpace(book.Quotes))
+            {
+                return string.Empty;
+            }
+
+            return string.Join("|", book.Quotes.Split('|').Select(q => q.Trim()));
+        }
+
+        protected override void OnInitialized()
+        {
+            EditContext = new EditContext(Book);
+            base.OnInitialized();
+        }
+
+    }
+}
--- MyBookshelf.Core/Model/Book.cs
+++ MyBookshelf.Core/Model/Book.cs
@@ -1,11 +1,14 @@
-﻿namespace MyBookshelf.Core.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyBookshelf.Core.Model
 {
     public class Book
     {
         public int Id { get; set; }
         public string? Title { get; set; }
         public string? Author { get; set; }
-        public int? Note { get; set; }
+        [Range(0, 5, ErrorMessage = "La note doit être un chiffre entre 0 et 5.")]
+        public int Note { get; set; }
         public string? Review { get; set; }
         public string? Quotes { get; set; }
     }
