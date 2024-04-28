c6c15309adca4f8e1f436c69f33569da12441a10
Mickael <mickaeldesclaux@gmail.com>
2024-04-28 12:24:30 +0200

    Handle Remove Book function

--- MyBookshelf.App/Components/Pages/HomeBase.cs
+++ MyBookshelf.App/Components/Pages/HomeBase.cs
@@ -15,5 +15,22 @@ namespace MyBookshelf.App.Components.Pages
             var bookList = await BookService.GetAllBooksAsync();
             Books = bookList.ToList();
         }
+
+        protected async Task RemoveBookAsync(Book book)
+        {
+            bool confirmed = await ShowDeleteConfirmationDialogAsync();
+            if (confirmed)
+            {
+                await BookService.RemoveBookAsync(book);
+                Books.Remove(book);
+            }
+        }
+
+        private async Task<bool> ShowDeleteConfirmationDialogAsync()
+        {
+            var result = await Application.Current.MainPage.DisplayAlert("Confirmation", "Voulez-vous vraiment supprimer ce livre?", "Oui", "Non");
+            return result;
+        }
+
     }
 }
